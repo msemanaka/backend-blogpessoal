a67a2df0332c35d04f527bc0719139a11ee6663f
hunterblp <milena.sforte@gmail.com>
2024-05-06 18:03:38 -0300

    Projeto Blog Pessoal

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { DevService } from './data/services/dev.service';
+import { PostagemModule } from './postagem/postagem.module';
+import { TemaModule } from './tema/tema.module';
+import { UsuarioModule } from './usuario/usuario.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: DevService,
+      imports: [ConfigModule],
+    }),
+    PostagemModule,
+    TemaModule,
+    AuthModule,
+    UsuarioModule
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule { }
